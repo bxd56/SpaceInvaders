1f57778732ed0d50ad661a0e668100256850c597
Arbia Bochra <aboubou@MacBook-Air-de-Arbia.local>
2025-11-12 11:36:48 +0100

    Organize files into folders

---
+++ src/model/model.c
@@ -0,0 +1,22 @@
+#include "model.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+//TODO Initialisation
+void initGame(){
+
+    Spaceship pl = {};
+    Player player = {.lives = 3 , .score = 0 , .spaceship = pl};
+
+}
+
+//TODO Gerer les collisions 
+void manageCollisions(){}
+//TODO Gerer du mouvemnt en groupe
+void manageEnemiesMovement(){}
+
+//TODO Gestion des niveaux
+void manageLevels(){}
+
+//TODO Gestion des scores et vies et collisions 
+void manageScoreLives(){}

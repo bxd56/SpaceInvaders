3f8b050230a1fb7fbb893e5d05300c921df26c93
shikazuka <159631726+shikazuka@users.noreply.github.com>
2025-11-27 15:42:38 +0100

    Refactor initGame and manageCollisions functions
    Updated initGame and manageCollisions functions to accept GameArea pointer and initialize game elements.

--- src/model/model.c
+++ src/model/model.c
@@ -3,15 +3,47 @@
 #include <stdlib.h>
 
 //TODO Initialisation
-void initGame(){
+void initGame(GameArea *game){
 
     Spaceship pl = {};
+    pl.state = true;
     Player player = {.lives = 3 , .score = 0 , .spaceship = pl};
+    game->player = player;
+    game->width = 100;
+    game->height = 100;
+
+    /*enemies initalisation*/
+
+    for(int i=0; i<MAX_ENEMIES;i++){
+        Spaceship enemy = {};
+        enemy.x = i*5;
+        enemy.y = 10;
+        enemy.speedX = 1;
+        enemy.speedY = 0;
+        enemy.state = true;
+        game->enemies[i] = enemy;
+    }
+
+    /*shields initialisation*/
+    for(int i=0; i<MAX_SHIELD;i++){
+        Shield shield = {};
+        shield.x = i*15;
+        shield.y = 80;
+        shield.resistance = 3;
+        shield.active = true;
+        game->shields[i] = shield;
+    }
+
+    game->game_over = false;
+
+
 
 }
 
 //TODO Gerer les collisions 
-void manageCollisions(){}
+void manageCollisions(GameArea *game){
+    
+}
 //TODO Gerer du mouvemnt en groupe
 void manageEnemiesMovement(){}
 
